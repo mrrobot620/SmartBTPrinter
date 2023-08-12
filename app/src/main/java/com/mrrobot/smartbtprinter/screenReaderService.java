c3f12c8441c5bff98e34c1f1bc98c44e413ca215
mrrobot620 <89240367+mrrobot620@users.noreply.github.com>
2023-08-12 14:40:43 +0530

    Moved Back to Old Way
    I don't want to merge these two way now

--- app/src/main/java/com/mrrobot/smartbtprinter/screenReaderService.java
+++ app/src/main/java/com/mrrobot/smartbtprinter/screenReaderService.java
@@ -24,42 +24,49 @@ public class screenReaderService extends AccessibilityService {
         Toast.makeText(this, "Accessibility Connected", Toast.LENGTH_LONG).show();
     }
 
-
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED) {
-            String packageName = event.getPackageName().toString();
-            if ("com.android.chrome".equals(packageName)) {
-                AccessibilityNodeInfo clickedNode = event.getSource();
-                if (clickedNode != null) {
-                    traverseAccessibilityTree(clickedNode);
-                    clickedNode.recycle();
+        AccessibilityNodeInfo mNodeInfo = event.getSource();
+        findTextWithStartingWords(mNodeInfo, "Keep this bag to");
+        findTextWithStartingWords(mNodeInfo, "Connection not");
+        findTextWithStartingWords(mNodeInfo, "Item already staged");
+    }
+
+    private String findTextWithStartingWords(AccessibilityNodeInfo mNodeInfo, String targetStartingWords) {
+        if (mNodeInfo == null) return null;
+        CharSequence nodeText = mNodeInfo.getText();
+        if (nodeText != null && nodeText.toString().startsWith(targetStartingWords)) {
+            return nodeText.toString();
+        }
+
+        if (mNodeInfo.getChildCount() < 1) return null;
+
+        for (int i = 0; i < mNodeInfo.getChildCount(); i++) {
+            String foundText = findTextWithStartingWords(mNodeInfo.getChild(i), targetStartingWords);
+            if (foundText != null) {
+                if (foundText.startsWith("Keep this bag to")) {
+
+                    // This is the code here I want to do the following instead of the line below , alos I think using regex would be better than substring
+
+//                    String gridCode = foundText.replaceAll("[^0-9]", "");
+
+                    String gridCode = regexFinder(":\\s*(\\w+)" , foundText);
+
+                    EventBus.getDefault().post(new SendGridDataEvent(gridCode));
+
+                }
+                else if(foundText.startsWith("Connection not")) {
+                    Log.d("HH", "Event Bus In ScreenReader: No Connection Found ");
+                    EventBus.getDefault().post(new com.mrrobot.rscsmartbtprinter.SendGridDataEvent("404"));
+                }
+                else if(foundText.startsWith("Item already staged")){
+                    Log.d("HH" , "Event Bus in Screen Reader: Item Already Staged");
+                    EventBus.getDefault().post(new SendGridDataEvent("405"));
                 }
             }
         }
+        return null; // Moved return statement outside of the loop
     }
-
-    private void traverseAccessibilityTree(AccessibilityNodeInfo node) {
-        if (node == null) {
-            return;
-        }
-
-        // Process text within this node
-        if (node.getText() != null) {
-            Log.d("Accessibility", "Text: " + node.getText());
-        }
-
-        // Traverse child nodes
-        for (int i = 0; i < node.getChildCount(); i++) {
-            AccessibilityNodeInfo childNode = node.getChild(i);
-            traverseAccessibilityTree(childNode);
-            childNode.recycle();
-        }
-    }
-
-
-
-
 
     @Override
     public void onInterrupt() {
